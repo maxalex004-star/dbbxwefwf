821004864bce8797a5b67f06af57cc38f9652054
maxalex004-star <maxalex004@gmail.com>
2025-10-08 17:41:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,23 @@
 from flask import Flask, request, jsonify
-
 from sklearn.linear_model import LinearRegression
-
 import numpy as np
-
- 
+import os
 
 app = Flask(__name__)
 
- 
-
 @app.route('/predict', methods=['POST'])
-
 def predict():
-
     try:
-
         data = request.get_json()
-
         months = np.array(data["months"]).reshape(-1, 1)
-
         values = np.array(data["values"])
-
         future = np.array(data["future"]).reshape(-1, 1)
-
- 
-
         model = LinearRegression()
-
         model.fit(months, values)
-
         predictions = model.predict(future)
-
- 
-
         return jsonify(predictions.tolist())
-
     except Exception as e:
-
         return jsonify({"error": str(e)}), 400
 
- 
-
 if __name__ == '__main__':
-
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
